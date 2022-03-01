584e862d6f9d33f7c4c0d5a029b3937cc10cb7ef
coding-geek21 <jayapritha9282@gmail.com>
2022-03-01 15:42:02 +0530

    updated

--- techsnap_automl/main_app/urls.py
+++ techsnap_automl/main_app/urls.py
@@ -7,4 +7,10 @@ urlpatterns = [
     path('problemtype/',views.problemType, name="problemType"),
     path('regression/',views.regression, name="regression"),
     path('classification/',views.classification, name="classification"),
+    path('xcolumns/',views.xcolumns,name="xcolumns"),
+    path('ycolumns/',views.ycolumns,name="ycolumns"),
+    path('xtest/',views.xtest,name="xtest"),
+    path('xtrain/',views.xtrain,name="xtrain"),
+    path('ytest/',views.ytest,name="ytest"),
+    path('ytrain/',views.ytrain,name="ytrain"),    
 ]
--- techsnap_automl/main_app/views.py
+++ techsnap_automl/main_app/views.py
@@ -2,13 +2,6 @@ from django.shortcuts import render, redirect
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from django.contrib import messages
-from .models import upload
-
-# Create your views here.
-cols=["a","b"]
-
-df=pd.DataFrame(columns=cols)
-
 
 def selectML(request):
     if "GET" == request.method:
@@ -18,6 +11,7 @@ def selectML(request):
         selectML=False
         ML_val = request.POST.get("mlval")
         if(ML_val == "Unsupervised Learning" or ML_val == "Reinforcement"):
+            selectML=True
             uploadfile=False
             messages.info(request,"Still Under Construction")
         else:
@@ -30,9 +24,13 @@ def uploadfile(request):
         return render(request, 'index.html', {})
     else:
         x_axis=[]
-        excel_file = request.FILES["excel_file"]
-        global df
-        df=pd.read_excel(excel_file)   
+        upload_file = request.FILES["upload_file"]
+        try:
+            df = pd.read_excel(upload_file)
+        except:
+            df = pd.read_csv(upload_file)
+        df = df.to_json()
+        request.session['df']=df
         problemType= True
         messages.success(request,"Successfully Uploaded file")
         messages.success(request,"Successfully Converted to DataFrame")
@@ -45,9 +43,12 @@ def problemType(request):
         probval=request.POST.get('probval')
         if(probval=="Regression"):
             regression=True
-            columns=df.columns
-            print(columns)
-            return render(request,'index.html',context={'regression':regression,'columns':columns})
+            if 'df' in request.session:
+                data=request.session['df']
+                df = pd.read_json(data)
+                columns=df.columns
+                print(columns)
+                return render(request,'index.html',context={'regression':regression,'columns':columns})
         elif(probval=="Classification"):
             classification=True
             return render(request,'index.html',context={'classification':classification})
@@ -58,30 +59,91 @@ def regression(request):
     x_axis=[]
     y_axis = request.POST.get("yaxis")
     testsize = request.POST.get("test_size")
-    columns=df.columns
-    for i in columns:
-        if(i != y_axis):
-            x_axis.append(i)
-    X=df.drop(y_axis,axis='columns')
-    y=df[y_axis]
-    X_train,X_test,y_train,y_test = train_test_split(X, y, test_size=testsize, random_state=20)
-    messages.success(request,"Successfully Splitted into four datasets")
-    return render(request,'index.html',{"yaxis":y_axis,"xaxis":x_axis,"xtrain":X_train,"xtest":X_test,"ytrain":y_train,"ytest":y_test})
+    if 'df' in request.session:
+        data=request.session['df']
+        df = pd.read_json(data)
+        columns=df.columns
+        for i in columns:
+            if(i != y_axis):
+                x_axis.append(i)
+        request.session['x-axis']=x_axis
+        request.session['y-axis']=y_axis
+        request.session['test_size']=testsize
+        X=df.drop(y_axis,axis='columns')
+        y=df[y_axis]
+        X_train,X_test,y_train,y_test = train_test_split(X, y, test_size=testsize, random_state=20)
+        messages.success(request,"Successfully Splitted into four datasets")
+        showbuttons=True
+        return render(request,'index.html',{'showbuttons':showbuttons})
 
 
 def classification(request):
     return render(request,'index.html')
 
-        # y_axis = request.POST.get("yaxis")
-        # testsize = request.POST.get("test_size")
-        # data=pd.read_excel(excel_file)
-        # print(data.head())
-        # columns=data.columns
-        # for i in columns:
-        #     if(i != y_axis):
-        #         x_axis.append(i)
-        # X=data.drop(y_axis,axis='columns')
-        # y=data[y_axis]
-        # X_train,X_test,y_train,y_test = train_test_split(X, y, test_size=testsize, random_state=20)
-        # print("Done Splitting")
-        #return render(request, 'index.html', {"yaxis":y_axis,"xaxis":x_axis,"xtrain":X_train,"xtest":X_test,"ytrain":y_train,"ytest":y_test})
+
+def xcolumns(request):
+    showbuttons=True
+    if 'x-axis' in request.session:
+        x_axis=request.session['x-axis']
+        return render(request,'index.html',{"xaxis":x_axis,"showbuttons":showbuttons})
+
+
+def ycolumns(request):
+    showbuttons=True
+    if 'y-axis' in request.session:
+        y_axis=request.session['y-axis']
+        return render(request,'index.html',{"yaxis":y_axis,"showbuttons":showbuttons})
+
+def xtest(request):
+    if 'df' in request.session and 'y-axis' in request.session:
+        data=request.session['df']
+        y_axis=request.session['y-axis']
+        testsize=request.session['test_size']
+        df = pd.read_json(data)
+        X=df.drop(y_axis,axis='columns')
+        y=df[y_axis]
+        X_train,X_test,y_train,y_test = train_test_split(X, y, test_size=testsize, random_state=20)
+        messages.success(request,"Successfully Splitted into four datasets")
+        showbuttons=True
+        return render(request,'index.html',{"xtest":X_test,'showbuttons':showbuttons})
+
+
+def ytest(request):
+    if 'df' in request.session and 'y-axis' in request.session:
+        data=request.session['df']
+        y_axis=request.session['y-axis']
+        testsize=request.session['test_size']
+        df = pd.read_json(data)
+        X=df.drop(y_axis,axis='columns')
+        y=df[y_axis]
+        X_train,X_test,y_train,y_test = train_test_split(X, y, test_size=testsize, random_state=20)
+        messages.success(request,"Successfully Splitted into four datasets")
+        showbuttons=True
+        return render(request,'index.html',{"ytest":y_test,'showbuttons':showbuttons})
+
+def xtrain(request):
+    if 'df' in request.session and 'y-axis' in request.session:
+        data=request.session['df']
+        y_axis=request.session['y-axis']
+        testsize=request.session['test_size']
+        df = pd.read_json(data)
+        X=df.drop(y_axis,axis='columns')
+        y=df[y_axis]
+        X_train,X_test,y_train,y_test = train_test_split(X, y, test_size=testsize, random_state=20)
+        messages.success(request,"Successfully Splitted into four datasets")
+        showbuttons=True
+        return render(request,'index.html',{"xtrain":X_train,'showbuttons':showbuttons})
+
+
+def ytrain(request):
+    if 'df' in request.session and 'y-axis' in request.session:
+        data=request.session['df']
+        y_axis=request.session['y-axis']
+        testsize=request.session['test_size']
+        df = pd.read_json(data)
+        X=df.drop(y_axis,axis='columns')
+        y=df[y_axis]
+        X_train,X_test,y_train,y_test = train_test_split(X, y, test_size=testsize, random_state=20)
+        messages.success(request,"Successfully Splitted into four datasets")
+        showbuttons=True
+        return render(request,'index.html',{"ytrain":y_train,'showbuttons':showbuttons})
